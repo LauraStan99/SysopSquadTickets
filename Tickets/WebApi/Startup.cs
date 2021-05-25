7e666f54cbd6f3710250d4fd5758ff536fb4e3c0
Stan Laura-Ioana <laurastan1999@gmail.com>
2021-05-25 19:34:46 +0300

    update

--- Tickets/WebApi/Startup.cs
+++ Tickets/WebApi/Startup.cs
@@ -33,7 +33,7 @@ namespace WebApi
 
             services.AddCors((options) =>
             {
-                options.AddPolicy("MyPolicy", builder => builder.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
+                options.AddPolicy("MyPolicy", builder => builder.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader().WithOrigins("http://localhost:3000"));
             });
 
             services.AddMvc(option => option.EnableEndpointRouting = false);
